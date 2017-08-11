a920a2b2c3b004b3a8ad838ca62febffbed8e7df
Sciguystfm <stefanmarchh@gmail.com>
2017-08-11 18:37:02 -0400

    Localized semantic.css, finished fetching state test

--- nanodropdown/src/index.js
+++ nanodropdown/src/index.js
@@ -11,26 +11,39 @@ export class ButtonWrapper extends React.Component {
 }
 export class DropdownWrapper extends React.Component {
     render() {
-
         const DropdownExampleSelection = props =>
             <Dropdown
                 placeholder="Options"
                 selection
                 options={props.options}
+                onChange={props.getSelected}
             />;
 
         return (
             <DropdownExampleSelection
                 options={this.props.options}
+                getSelected={this.props.getSelected}
             />
         );
     }
 }
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: "Hello!"
+        };
+    }
     render() {
+        const getSelected = (event, data) => {
+            this.setState({ selected: data.value });
+        };
         return (
             <div>
+                <h1>
+                    {this.state.selected}
+                </h1>
                 <DropdownWrapper
                     options={[
                         {
@@ -46,6 +59,7 @@ class App extends React.Component {
                             value: 3
                         }
                     ]}
+                    getSelected={getSelected}
                 />
                 <ButtonWrapper />
             </div>
